6f38453df2f6a33fe265e3a2638e9e10804e2768
Bart Verdonck <bverdonck@reference.be>
2018-05-27 23:00:09 +0200

    Added file size and content type validators

---
+++ src/Feature/FormsExtensions/code/Fields/FileUpload/ContentTypeValidation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.ExperienceForms.Mvc.Models.Validation;
+
+namespace Feature.FormsExtensions.Fields.FileUpload
+{
+    public class ContentTypeValidation : ValidationElement<string>
+    {
+        protected string AllowedContentTypes { get; set; }
+
+        public ContentTypeValidation(ValidationDataModel validationItem) : base(validationItem)
+        {
+
+        }
+
+        public override void Initialize(object validationModel)
+        {
+            base.Initialize(validationModel);
+            if (!(validationModel is FileUploadModel fileUploadModel))
+                return;
+            AllowedContentTypes = fileUploadModel.AllowedContentTypes;
+        }
+
+        public override ValidationResult Validate(object value)
+        {
+            var postedFile = (HttpPostedFileBase) value;
+            if (AllowedContentTypes.Contains(postedFile.ContentType))
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult(FormatMessage(postedFile.ContentType));
+        }
+                
+
+        public override IEnumerable<ModelClientValidationRule> ClientValidationRules
+        {
+            get
+            {
+                var rule = new ModelClientValidationRule
+                {
+                    ErrorMessage = FormatMessage(),
+                    ValidationType = "contenttype"
+                };
+                rule.ValidationParameters["allowedcontenttypes"] = AllowedContentTypes;
+                yield return rule;
+            }
+        }
+    }
+}
--- src/Feature/FormsExtensions/code/Fields/FileUpload/FileUploadModel.cs
+++ src/Feature/FormsExtensions/code/Fields/FileUpload/FileUploadModel.cs
@@ -1,14 +1,36 @@
-﻿using System.Web;
+﻿using System.Globalization;
+using System.Web;
 using Feature.FormsExtensions.Business.FileUpload;
+using Sitecore;
+using Sitecore.Data.Items;
 using Sitecore.ExperienceForms.Mvc.Models.Validation;
 
 namespace Feature.FormsExtensions.Fields.FileUpload
 {
     public class FileUploadModel : ValueNotValidatedInputViewModel<IStoredFile>
     {
+        private const string AllowedContentTypesParam = "Allowed Content Types";
+        private const string MaxFileSizeParam = "Max File Size";
+
         [DynamicRequired]
         [DynamicValidation]
         public virtual HttpPostedFileBase File { get; set; }
 
+        public string AllowedContentTypes { get; set; }
+        public int MaxFileSize { get; set; }
+
+        protected override void InitItemProperties(Item item)
+        {
+            base.InitItemProperties(item);
+            AllowedContentTypes = StringUtil.GetString(item.Fields[AllowedContentTypesParam]);
+            MaxFileSize = MainUtil.GetInt(item.Fields[MaxFileSizeParam]?.Value, 0);
+        }
+
+        protected override void UpdateItemFields(Item item)
+        {
+            base.UpdateItemFields(item);
+            item.Fields[AllowedContentTypesParam]?.SetValue(AllowedContentTypes, true);
+            item.Fields[MaxFileSizeParam]?.SetValue(MaxFileSize.ToString(CultureInfo.InvariantCulture),true);
+        }
     }
 }
