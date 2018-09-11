d92300bd8ee241302dda94717244a148eb8af42c
Bart Verdonck <bverdonck@reference.be>
2018-09-11 13:52:49 +0200

    Added hidden field

---
+++ src/Feature/FormsExtensions/code/Fields/Hidden/HiddenViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Feature.FormsExtensions.Fields.Bindings;
+using Sitecore.Data.Items;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+
+namespace Feature.FormsExtensions.Fields.Hidden
+{
+    public class HiddenViewModel : InputViewModel<string>, IBindingSettings
+    {
+        public string BindingToken { get; set; }
+        public bool PrefillBindingValue { get; set; }
+        public bool StoreBindingValue { get; set; }
+
+        protected override void InitItemProperties(Item item)
+        {
+            base.InitItemProperties(item);
+            this.InitBindingSettingsProperties(item);
+        }
+
+        protected override void UpdateItemFields(Item item)
+        {
+            base.UpdateItemFields(item);
+            this.UpdateBindingSettingsFields(item);
+        }
+    }
+}
