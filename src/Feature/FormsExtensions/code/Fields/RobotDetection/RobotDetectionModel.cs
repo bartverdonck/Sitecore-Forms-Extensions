96394506fb3758dcc8e9012e5e45397520bcfce6
Bart Verdonck <bverdonck@reference.be>
2019-11-30 21:03:37 +0100

    Added robot detection component

---
+++ src/Feature/FormsExtensions/code/Fields/RobotDetection/RobotDetectionModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Mvc.Models.Fields;
+using Sitecore.ExperienceForms.ValueProviders;
+
+namespace Feature.FormsExtensions.Fields.RobotDetection
+{
+    [Serializable]
+    public class RobotDetectionModel : FieldViewModel, IValueField
+    {
+        [RobotDetectionValidation(ErrorMessage = "robotdetection.robotdetected")]
+        public string DummyValue { get; set; }
+
+        public void InitializeValue(FieldValueProviderContext context)
+        {
+            //Irrelevant for robotdetection.
+        }
+
+        public bool Required { get; set; }
+        public bool IsTrackingEnabled { get; set; }
+        public bool AllowSave { get; set; }
+    }
+}
---
+++ src/Feature/FormsExtensions/code/Fields/RobotDetection/RobotDetectionValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Sitecore.ExperienceForms.RobotDetection;
+using Sitecore.Globalization;
+
+namespace Feature.FormsExtensions.Fields.RobotDetection
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RobotDetectionValidationAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return RobotDetectionHelper.IsContactClassificationGuessed;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return Translate.Text(this.ErrorMessageString);
+        }
+    }
+}
