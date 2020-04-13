2c56f1d1ba694c26685d1e3048800bb1c15bc586
VERDO-DESK1\Bart Verdonck <bart.verdonck@gmail.com>
2020-04-13 15:20:56 +0200

    Removed deployment folder

--- deployment/Scripts/Utilities/convert-module.ps1
+++
@@ -1,5 +0,0 @@
-﻿$Origin = "c:\Projects\Sitecore-Forms-Extensions\Source\downloads\Sitecore 9.3.x\Sitecore Forms Extensions for Sitecore 9.3-3.0.zip"
-$Destination = "c:\Projects\Sitecore-Forms-Extensions\Source\downloads\Sitecore 9.3.x\"
-
-Import-Module "C:\Software\Azure Toolkit\Sitecore Azure Toolkit 2.0.0 rev.171010\tools\Sitecore.Cloud.Cmdlets.dll" -Verbose 
-$wdpPath = ConvertTo-SCModuleWebDeployPackage -Path $Origin -Destination $Destination -Force
