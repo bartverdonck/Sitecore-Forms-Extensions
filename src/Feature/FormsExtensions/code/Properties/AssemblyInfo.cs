87a9baa47c09b98897848fd44ad072b0996d3b5f
Bart Verdonck <bart.verdonck@gmail.com>
2020-06-07 16:48:46 +0200

    #79

--- src/Feature/FormsExtensions/code/Properties/AssemblyInfo.cs
+++ src/Feature/FormsExtensions/code/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Feature.FormsExtentions")]
 [assembly: Guid("df75115a-6f76-41c6-b61f-3772e523e78e")]
+[assembly: AssemblyVersion("3.0")]
