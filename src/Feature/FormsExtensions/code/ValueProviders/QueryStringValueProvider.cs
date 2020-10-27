ebe1ece1b458f7226b74223ee559d5abcf2c2782
Bart Verdonck <bverdonck@reference.be>
2020-10-27 08:51:15 +0100

    Add QueryString value provider. Added layouting on testpages.

---
+++ src/Feature/FormsExtensions/code/ValueProviders/QueryStringValueProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using Sitecore.ExperienceForms.ValueProviders;
+
+namespace Feature.FormsExtensions.ValueProviders
+{
+    public class QueryStringValueProvider : IFieldValueProvider
+    {
+        public object GetValue(string parameter)
+        {
+            var queryParam = parameter;
+            if (string.IsNullOrEmpty(queryParam))
+            {
+                queryParam = ValueProviderContext.FieldItem.Name;
+            }
+            var value = HttpContext.Current.Request.QueryString[queryParam];
+            return value;
+        }
+
+        public FieldValueProviderContext ValueProviderContext { get; set; }
+    }
+}
