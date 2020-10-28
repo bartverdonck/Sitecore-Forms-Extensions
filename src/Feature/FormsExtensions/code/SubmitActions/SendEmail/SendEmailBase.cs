9e4d296e1d350dd131d053531762b865c6675ceb
Bart Verdonck <bverdonck@reference.be>
2020-10-28 21:03:55 +0100

    #85 fallback for sendmail on cm

--- src/Feature/FormsExtensions/code/SubmitActions/SendEmail/SendEmailBase.cs
+++ src/Feature/FormsExtensions/code/SubmitActions/SendEmail/SendEmailBase.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
 using Feature.FormsExtensions.SubmitActions.SendEmail.Tokens;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
 using Sitecore.EmailCampaign.Cd.Actions;
 using Sitecore.EmailCampaign.Cd.Services;
 using Sitecore.EmailCampaign.Model.Messaging;
+using Sitecore.EmailCampaign.Model.Messaging.Buses;
 using Sitecore.ExM.Framework.Diagnostics;
 using Sitecore.ExperienceForms.Models;
 using Sitecore.ExperienceForms.Processing;
 using Sitecore.ExperienceForms.Processing.Actions;
+using Sitecore.Framework.Messaging;
 using Sitecore.XConnect;
 
 namespace Feature.FormsExtensions.SubmitActions.SendEmail
@@ -60,7 +64,19 @@ namespace Feature.FormsExtensions.SubmitActions.SendEmail
             automatedMessage.MessageId = messageId;
             automatedMessage.CustomTokens = customTokens;
             automatedMessage.TargetLanguage = Sitecore.Context.Language.Name;
-            clientApiService.SendAutomatedMessage(automatedMessage);
+            SendAutomatedMessage(automatedMessage);
+        }
+
+        private void SendAutomatedMessage(AutomatedMessage automatedMessage)
+        {
+            if(clientApiService!=null){
+                clientApiService.SendAutomatedMessage(automatedMessage);
+            }
+            else
+            {
+                var automatedMessageBus = ServiceLocator.ServiceProvider.GetService<IMessageBus<AutomatedMessagesBus>>();
+                automatedMessageBus.Send(automatedMessage);
+            }
         }
 
         protected virtual Dictionary<string, object> BuildCustomTokens(T data, FormSubmitContext formSubmitContext)
