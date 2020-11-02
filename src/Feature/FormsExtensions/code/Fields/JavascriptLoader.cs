88f432762e7a11a7f7c7ccb97f4d45c46fb3a8b9
Bart Verdonck <bverdonck@reference.be>
2020-11-02 18:54:43 +0100

    #87 fix for captcha on second page

---
+++ src/Feature/FormsExtensions/code/Fields/JavascriptLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.ExperienceForms.Mvc.Constants;
+using Sitecore.ExperienceForms.Mvc.Pipelines.RenderFields;
+using Sitecore.Mvc.Pipelines;
+
+namespace Feature.FormsExtensions.Fields
+{
+    public class JavascriptLoader : MvcPipelineProcessor<RenderFieldsEventArgs>
+    {
+        public override void Process(RenderFieldsEventArgs args)
+        {
+            var scripts = HttpContext.Current.Items[IncludeFileKeys.ScriptsFilesKey] as Dictionary<string, IHtmlString>;
+
+            var scriptKey = "formextensions-validate";
+            if (scripts != null && !scripts.ContainsKey(scriptKey))
+            {
+                scripts.Add(scriptKey, new MvcHtmlString("<script src=\"/sitecore%20modules/Web/ExperienceForms/scripts/formsextensions.validate.js\"></script>"));
+            }
+        }
+    }
+}
