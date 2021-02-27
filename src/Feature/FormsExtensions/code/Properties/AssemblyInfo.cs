cebd804bfcc34af575213541f8709cab05f579ae
Bart Verdonck <bverdonck@reference.be>
2021-02-27 19:37:45 +0100

    Bump assemblyinfo

--- src/Feature/FormsExtensions/code/Properties/AssemblyInfo.cs
+++ src/Feature/FormsExtensions/code/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SitecoreFormsExtentions.Core")]
 [assembly: Guid("df75115a-6f76-41c6-b61f-3772e523e78e")]
-[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
 [assembly: AssemblyCompany("Bart Verdonck")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyDescription("Sitecore Forms Extensions is a module to enrich the default Sitecore Forms module. This library does not intall the module, it only provides the classes and interfaces to extends on Sitecore Forms Extentions.")]
